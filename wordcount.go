f517df8f4b12068af0c6a7ec60a4924a2e56aefb
namikaze-dev <usmansadik96@gmail.com>
2023-03-27 16:28:21 +0100

    Add func to count all (lines, words, chars) in a file from fs

--- wordcount.go
+++ wordcount.go
@@ -2,10 +2,16 @@ package main
 
 import (
 	"bufio"
+	"bytes"
+	"io"
 	"io/fs"
 	"os"
 	"strings"
 )
+
+type CountResult struct {
+	Lines, Words, Chars int
+}
 
 func CountLinesFromFs(fileSys fs.FS, fn string) (int, error) {
 	f, err := openFile(fileSys, fn)
@@ -31,6 +37,14 @@ func CountCharsFromFs(fileSys fs.FS, fn string) (int, error) {
 	return countChars(f), nil
 }
 
+func CountAllFromFs(fileSys fs.FS, fn string) (CountResult, error) {
+	f, err := openFile(fileSys, fn)
+	if err != nil {
+		return CountResult{}, err
+	}
+	return countAll(f), nil
+}
+
 func openFile(fileSys fs.FS, fn string) (fs.File, error) {
 	f, err := fileSys.Open(fn)
 	if err != nil {
@@ -49,7 +63,7 @@ func openFile(fileSys fs.FS, fn string) (fs.File, error) {
 	return f, nil
 }
 
-func countLines(f fs.File) int {
+func countLines(f io.Reader) int {
 	var count int
 	scn := bufio.NewScanner(f)
 	for scn.Scan() {
@@ -58,7 +72,7 @@ func countLines(f fs.File) int {
 	return count
 }
 
-func countWords(f fs.File) int {
+func countWords(f io.Reader) int {
 	var count int
 	scn := bufio.NewScanner(f)
 	for scn.Scan() {
@@ -67,7 +81,7 @@ func countWords(f fs.File) int {
 	return count
 }
 
-func countChars(f fs.File) int {
+func countChars(f io.Reader) int {
 	var count int
 	scn := bufio.NewScanner(f)
 	scn.Split(bufio.ScanBytes)
@@ -75,4 +89,25 @@ func countChars(f fs.File) int {
 		count++
 	}
 	return count
+}
+
+func countAll(r io.Reader) CountResult {
+	// use io.TeeReader to duplicate r
+	var buf = &bytes.Buffer{}
+	rd := io.TeeReader(r, buf)
+	lines, words := countLinesAndWords(rd)
+	return CountResult{
+		Lines: lines,
+		Words: words,
+		Chars: countChars(buf),
+	}
+}
+
+func countLinesAndWords(f io.Reader) (lines int, words int) {
+	scn := bufio.NewScanner(f)
+	for scn.Scan() {
+		lines += 1
+		words += len(strings.Fields(scn.Text()))
+	}
+	return
 }
--- wordcount_test.go
+++ wordcount_test.go
@@ -2,8 +2,9 @@ package main_test
 
 import (
 	"errors"
-	"os"
 	"io/fs"
+	"os"
+	"reflect"
 	"testing"
 	"testing/fstest"
 
@@ -122,6 +123,49 @@ func TestCountChars(t *testing.T) {
 	
 	t.Run("failure permission denied", func(t *testing.T) {
 		_, err := main.CountCharsFromFs(FailingPermFs{}, "")
+		if !errors.Is(err, os.ErrPermission) {
+			t.Fatalf("want error %v got error %v", os.ErrPermission, err)
+		}
+	})
+}
+
+func TestCountAll(t *testing.T) {
+	t.Run("success", func(t *testing.T) {
+		fn := "file.txt"
+		got, err := main.CountAllFromFs(testFS, fn)
+		if err != nil {
+			t.Fatalf("unexpected error from main.CountAllFromFs: %v", err)
+		}
+
+		want := main.CountResult{
+			Lines: 4,
+			Words: 9,
+			Chars: 33,
+		}
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %+v want %+v", got, want)
+		}
+	})
+	
+	t.Run("failure file doesn't exist", func(t *testing.T) {
+		fn := "non-existing-file.txt"
+		_, err := main.CountAllFromFs(testFS, fn)
+		if !errors.Is(err, os.ErrNotExist) {
+			t.Fatalf("want error %v got error %v", os.ErrNotExist, err)
+		}
+	})
+
+	
+	t.Run("failure is a directory", func(t *testing.T) {
+		fn := "dir"
+		_, err := main.CountAllFromFs(testFS, fn)
+		if !errors.Is(err, os.ErrInvalid) {
+			t.Fatalf("want error %v got error %v", os.ErrInvalid, err)
+		}
+	})
+	
+	t.Run("failure permission denied", func(t *testing.T) {
+		_, err := main.CountAllFromFs(FailingPermFs{}, "")
 		if !errors.Is(err, os.ErrPermission) {
 			t.Fatalf("want error %v got error %v", os.ErrPermission, err)
 		}
